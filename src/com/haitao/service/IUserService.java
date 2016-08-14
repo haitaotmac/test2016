b4c9ff1870200bb87e730f8f27d9f07425ee98de
haitao <haitaojodan@126.com>
2016-08-14 23:13:35 +0800

    create UserServiceImpl

--- src/com/haitao/service/IUserService.java
+++ src/com/haitao/service/IUserService.java
@@ -1,5 +1,7 @@
 package com.haitao.service;
 
-public interface IUserService {
+import com.haitao.domain.User;
 
+public interface IUserService {
+	public void add(User user);
 }
---
+++ src/com/haitao/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.haitao.service.impl;
+
+import com.haitao.domain.User;
+import com.haitao.service.IUserService;
+
+public class UserServiceImpl implements IUserService{
+
+	@Override
+	public void add(User user) {
+		
+	}
+
+}
