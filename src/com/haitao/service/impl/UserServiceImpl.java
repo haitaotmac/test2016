4faa1beca0e8d94cbf7c7870a1d49591b35e7a88
haitao <haitaojodan@126.com>
2016-08-14 23:59:56 +0800

    he bin liangzhe xiug de

--- src/com/haitao/service/impl/UserServiceImpl.java
+++ src/com/haitao/service/impl/UserServiceImpl.java
@@ -1,28 +1,30 @@
 package com.haitao.service.impl;
 
+import com.haitao.dao.IUserDao;
 import com.haitao.domain.User;
 import com.haitao.service.IUserService;
 
 public class UserServiceImpl implements IUserService{
-
+	private IUserDao userDao;
+	
 	@Override
 	public void add(User user) {
-		
+		this.userDao.addUser(user);
 	}
 
 	@Override
 	public void delete(User user) {
-		
+		this.userDao.deleteUserById(user.getId());
 	}
 
 	@Override
 	public void edit(User user) {
-		
+		this.userDao.editUser(user);
 	}
 
 	@Override
 	public User findUserById(Integer id) {
-		return null;
+		return this.userDao.findUserById(id);
 	}
 
 }
