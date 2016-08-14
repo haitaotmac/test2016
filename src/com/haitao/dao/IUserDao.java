ebb0c5c5f821e848ef098e1ddf4e15a720e07408
lina <lina@126.com>
2016-08-14 23:16:25 +0800

    add userDao

---
+++ src/com/haitao/dao/IUserDao.java
@@ -0,0 +1,7 @@
+package com.haitao.dao;
+
+import com.haitao.domain.User;
+
+public interface IUserDao {
+	public void addUser(User user);
+}
---
+++ src/com/haitao/dao/impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.haitao.dao.impl;
+
+import com.haitao.dao.IUserDao;
+import com.haitao.domain.User;
+
+public class UserDaoImpl implements IUserDao{
+
+	@Override
+	public void addUser(User user) {
+		
+	}
+
+}
